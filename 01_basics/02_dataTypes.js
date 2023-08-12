b4b2b9978cd0fd82dac75f5dd17bfb034f610445
Mrinal-01 <78723042+Mrinal-01@users.noreply.github.com>
2023-08-12 12:22:50 +0000

    Data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all js code as newer version
+/*
+1. You must have to write "use strict" in first line before write anything
+2. alert()  //You can't use it as you are using node js not browser
+*/
+let name="Mrinal"
+let age=27
+let isLoggedIn=false
+
+//number => 2 to power of 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value. null is an object
+//undefined => Value not define
+//symbol => for use uniqueness, mainly used in Reat
+//object
+
+//typeOf :- It will retunr the typeOf variable
+ console.table([typeOf(undefined),typeOf("Mrinal"),typeOf(age)])
+console.log(typeof undefined)
